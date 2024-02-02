eefb467dd3d2b3938be77df710f0254e21f895c2
meter <1462068546@qq.com>
2024-02-02 20:54:48 +0800

    refactor ps1b

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -1,17 +1,27 @@
-portion_down_payment = 0.25
-current_savings = 0.0
-r = 0.04
-annual_salary = float(input("Enter your starting annual salary:"))
-portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
-total_cost = float(input("Enter the cost of your dream home:"))
-semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal:"))
+def calculate_months_for_down_payment(annual_salary, portion_saved, total_cost, semi_annual_raise):
+    global current_savings
+    months = 0
+    while current_savings < total_cost * portion_down_payment:
+        current_savings += current_savings * r / 12
+        current_savings += annual_salary * portion_saved / 12
+        months += 1
+        if months % 6 == 0:
+            annual_salary += annual_salary * semi_annual_raise
+    return months
 
-months = 0
-while current_savings < total_cost * portion_down_payment:
-    current_savings += current_savings * r / 12
-    current_savings += annual_salary * portion_saved / 12
-    months += 1
-    if months % 6 == 0:
-        annual_salary += annual_salary * semi_annual_raise
 
-print("Number of months:", months)
+def main():
+    annual_salary = float(input("Enter your annual salary: "))
+    portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+    total_cost = float(input("Enter the cost of your dream home: "))
+    semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
+    months = calculate_months_for_down_payment(annual_salary, portion_saved, total_cost, semi_annual_raise)
+    print("Number of months: ", months)
+
+
+if __name__ == '__main__':
+    portion_down_payment = 0.25
+    current_savings = 0.0
+    r = 0.04
+    main()
+
