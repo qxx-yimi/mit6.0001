7cfe9d0a4c6ac6d6ca72aa8997c19dbf517f2440
meter <1462068546@qq.com>
2024-02-15 22:33:24 +0800

    problem 2:dealing with hands

--- PS3/ps3.py
+++ PS3/ps3.py
@@ -178,9 +178,12 @@ def update_hand(hand, word):
     hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
-
-    pass  # TO DO... Remove this line when you implement this function
-
+    word = word.lower()
+    new_hand = hand.copy()
+    for letter in word:
+        if new_hand.get(letter, 0) != 0:
+            new_hand[letter] -= 1
+    return new_hand
 
 #
 # Problem #3: Test word validity
